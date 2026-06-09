791f4198d13a3d7fecbfbcabc02b9fd145a3903e
anton effendi <wuyouduan@gmail.com>
2026-06-09 15:40:52 +0700

    file v0.0.2

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/wuyouduan/app-golang-say-hello
+
+go 1.23.7
+
+require github.com/wuyouduan/golang-say-hello v0.0.2
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	golang_say_hello "github.com/wuyouduan/golang-say-hello"
+)
+
+func main() {
+	fmt.Println(golang_say_hello.SayHello() + " Anton")
+}
